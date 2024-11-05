4f9c64a87a25abcbfe8b2b485f6461da7ecb8550
rtthw <172864013+rtthw@users.noreply.github.com>
2024-11-05 07:25:10 -0500

    Setup block widget

--- common/src/widgets.rs
+++ common/src/widgets.rs
@@ -1,3 +1,26 @@
 //! Widgets
 
 
+
+use dreg::prelude::*;
+
+
+
+pub struct Block;
+
+impl Block {
+    pub fn render(self, area: Rect, buf: &mut Buffer) {
+        for y in area.top()..area.bottom() {
+            buf.get_mut(area.x, y).set_char('│');
+            buf.get_mut(area.right().saturating_sub(1), y).set_char('│');
+        }
+        for x in area.left()..area.right() {
+            buf.get_mut(x, area.y).set_char('─');
+            buf.get_mut(x, area.bottom().saturating_sub(1)).set_char('─');
+        }
+        buf.get_mut(area.x, area.y).set_char('┌');
+        buf.get_mut(area.right().saturating_sub(1), area.y).set_char('┐');
+        buf.get_mut(area.right().saturating_sub(1), area.bottom().saturating_sub(1)).set_char('┘');
+        buf.get_mut(area.x, area.bottom().saturating_sub(1)).set_char('└');
+    }
+}
